4c7b4cc12a4d8ebca74b8c290cc088c7362ae144
dcwang <dcwang@zkbsn.com>
2022-05-17 19:36:41 +0800

    提交

---
+++ shop-commodity/src/main/java/com/shop/commodity/CommodityApplication.java
@@ -0,0 +1,16 @@
+package com.shop.commodity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: dcWang
+ * @Date: 2022/5/17 19:18
+ * @Description:
+ */
+@SpringBootApplication
+public class CommodityApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CommodityApplication.class);
+    }
+}
