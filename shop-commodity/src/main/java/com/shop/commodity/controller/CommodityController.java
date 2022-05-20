6af22045028db421ce28f4cbb989b4396610dd95
dcwang <dcwang@zkbsn.com>
2022-05-20 18:42:30 +0800

    提交

--- shop-commodity/src/main/java/com/shop/commodity/controller/CommodityController.java
+++ shop-commodity/src/main/java/com/shop/commodity/controller/CommodityController.java
@@ -4,6 +4,7 @@ import com.shop.commodity.entity.Commodity;
 import com.shop.commodity.service.ICommodityService;
 import com.shop.common.util.Result;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,6 +20,7 @@ import java.util.List;
 @Slf4j
 @RestController
 @RequestMapping("/commodity")
+@CrossOrigin
 public class CommodityController {
 
     @Resource
